f4fc5d7e2b322c0ddbfdf0ef361754d8a36abad1
VatsalDave2102 <vatsaldave2002@gmail.com>
2024-04-01 22:09:29 +0530

    add register method to input fields

--- src/components/YouTubeForm.tsx
+++ src/components/YouTubeForm.tsx
@@ -2,17 +2,23 @@ import { useForm } from "react-hook-form";
 
 const YouTubeForm = () => {
 	const form = useForm();
+
+	// register lets us register on form fields
+	const { register } = form;
+
+	// register itself return 4 properties, rather than this destructure register on the input itself
+	// const { name, ref, onBlur, onChange } = register("username");
 	return (
 		<div className="container mx-auto pt-10">
 			<form className="flex flex-col gap-2">
 				<label htmlFor="username">Username</label>
-				<input type="text" id="username" name="username" />
+				<input type="text" id="username" {...register("username")} />
 
 				<label htmlFor="email">Email</label>
-				<input type="email" id="email" name="email" />
+				<input type="email" id="email" {...register("email")} />
 
 				<label htmlFor="channel">Channel</label>
-				<input type="text" id="channel" name="channel" />
+				<input type="text" id="channel" {...register("username")} />
 				<div>
 					<button className="p-2 bg-blue-900 text-white rounded-md hover:bg-blue-500">
 						Submit

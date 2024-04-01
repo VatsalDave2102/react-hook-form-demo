295c1a4a18e0669a967929344f83c01b970947b5
VatsalDave2102 <vatsaldave2002@gmail.com>
2024-04-01 20:41:38 +0530

    youtube form setup

---
+++ src/components/YouTubeForm.tsx
@@ -0,0 +1,23 @@
+const YouTubeForm = () => {
+	return (
+		<div className="container mx-auto pt-10">
+			<form className="flex flex-col gap-2">
+				<label htmlFor="username">Username</label>
+				<input type="text" id="username" name="username" />
+
+				<label htmlFor="email">Email</label>
+				<input type="email" id="email" name="email" />
+
+				<label htmlFor="channel">Channel</label>
+				<input type="text" id="channel" name="channel" />
+				<div>
+					<button className="p-2 bg-blue-900 text-white rounded-md hover:bg-blue-500">
+						Submit
+					</button>
+				</div>
+			</form>
+		</div>
+	);
+};
+
+export default YouTubeForm;

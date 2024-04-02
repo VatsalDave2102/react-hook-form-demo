6e4e37e996ab108d1a43c55521f7a72ee3c1d4f9
Vatsal Dave <vatsaldave2002@gmail.com>
2024-04-02 15:15:17 +0530

    add field validation

--- src/components/YouTubeForm.tsx
+++ src/components/YouTubeForm.tsx
@@ -20,15 +20,42 @@ const YouTubeForm = () => {
 	// const { name, ref, onBlur, onChange } = register("username");
 	return (
 		<div className="container mx-auto pt-10">
-			<form className="flex flex-col gap-2" onSubmit={handleSubmit(onSubmit)}>
+			<form
+				className="flex flex-col gap-2"
+				onSubmit={handleSubmit(onSubmit)}
+				noValidate
+			>
 				<label htmlFor="username">Username</label>
-				<input type="text" id="username" {...register("username")} />
+				<input
+					type="text"
+					id="username"
+					{...register("username", {
+						required: {
+							value: true,
+							message: "Username is required",
+						},
+					})}
+				/>
 
 				<label htmlFor="email">Email</label>
-				<input type="email" id="email" {...register("email")} />
+				<input
+					type="email"
+					id="email"
+					{...register("email", {
+						pattern: {
+							value:
+								/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g,
+							message: "Invalid email format",
+						},
+					})}
+				/>
 
 				<label htmlFor="channel">Channel</label>
-				<input type="text" id="channel" {...register("channel")} />
+				<input
+					type="text"
+					id="channel"
+					{...register("channel", { required: "Username is required" })}
+				/>
 				<div>
 					<button className="p-2 bg-blue-900 text-white rounded-md hover:bg-blue-500">
 						Submit

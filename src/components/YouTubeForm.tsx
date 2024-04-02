4a3c3853e719bc0aee4750e0d70cd4c86705f608
Vatsal Dave <vatsaldave2002@gmail.com>
2024-04-02 15:06:43 +0530

    add submit handler

--- src/components/YouTubeForm.tsx
+++ src/components/YouTubeForm.tsx
@@ -1,17 +1,26 @@
 import { useForm } from "react-hook-form";
 import { DevTool } from "@hookform/devtools";
 
+type FormValues = {
+	username: string;
+	email: string;
+	channel: string;
+};
 const YouTubeForm = () => {
-	const form = useForm();
+	const form = useForm<FormValues>();
 
 	// register lets us register on form fields
-	const { register, control } = form;
+	const { register, control, handleSubmit } = form;
+
+	const onSubmit = (formdata: FormValues) => {
+		console.log("Form submitted", formdata);
+	};
 
 	// register itself return 4 properties, rather than this destructure register on the input itself
 	// const { name, ref, onBlur, onChange } = register("username");
 	return (
 		<div className="container mx-auto pt-10">
-			<form className="flex flex-col gap-2">
+			<form className="flex flex-col gap-2" onSubmit={handleSubmit(onSubmit)}>
 				<label htmlFor="username">Username</label>
 				<input type="text" id="username" {...register("username")} />
 
@@ -19,7 +28,7 @@ const YouTubeForm = () => {
 				<input type="email" id="email" {...register("email")} />
 
 				<label htmlFor="channel">Channel</label>
-				<input type="text" id="channel" {...register("username")} />
+				<input type="text" id="channel" {...register("channel")} />
 				<div>
 					<button className="p-2 bg-blue-900 text-white rounded-md hover:bg-blue-500">
 						Submit

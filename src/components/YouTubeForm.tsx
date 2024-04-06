20cd0109f699286d26d68794ded19a326ceeddf2
VatsalDave2102 <vatsaldave2002@gmail.com>
2024-04-06 13:10:18 +0530

    add custom validation

--- src/components/YouTubeForm.tsx
+++ src/components/YouTubeForm.tsx
@@ -5,6 +5,7 @@ type FormValues = {
 	username: string;
 	email: string;
 	channel: string;
+	social: { twitter: string; facebook: string };
 };
 
 const YouTubeForm = () => {
@@ -18,7 +19,10 @@ const YouTubeForm = () => {
 	});
 
 	// register lets us register on form fields
-	const { register, control, handleSubmit } = form;
+	// handlerSubmit takes a submit method, it validate input and passes the values to submit method
+	// formState has many properties we are using it here to show error messages
+	const { register, control, handleSubmit, formState } = form;
+	const { errors } = formState;
 
 	const onSubmit = (formdata: FormValues) => {
 		console.log("Form submitted", formdata);
@@ -33,37 +37,50 @@ const YouTubeForm = () => {
 				onSubmit={handleSubmit(onSubmit)}
 				noValidate
 			>
-				<label htmlFor="username">Username</label>
-				<input
-					type="text"
-					id="username"
-					{...register("username", {
-						required: {
-							value: true,
-							message: "Username is required",
-						},
-					})}
-				/>
-
-				<label htmlFor="email">Email</label>
-				<input
-					type="email"
-					id="email"
-					{...register("email", {
-						pattern: {
-							value:
-								/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g,
-							message: "Invalid email format",
-						},
-					})}
-				/>
-
-				<label htmlFor="channel">Channel</label>
-				<input
-					type="text"
-					id="channel"
-					{...register("channel", { required: "Username is required" })}
-				/>
+				<div className="form-control">
+					<label htmlFor="username">Username</label>
+					<input
+						type="text"
+						id="username"
+						{...register("username", {
+							required: {
+								value: true,
+								message: "Username is required",
+							},
+							validate: (value) => {
+								return (
+									value !== "admin@example.com" ||
+									"Enter a different email address"
+								);
+							},
+						})}
+					/>
+					<p className="text-red-500">{errors.username?.message}</p>
+				</div>
+				<div className="form-control">
+					<label htmlFor="email">Email</label>
+					<input
+						type="email"
+						id="email"
+						{...register("email", {
+							pattern: {
+								value:
+									/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g,
+								message: "Invalid email format",
+							},
+						})}
+					/>
+					<p className="text-red-500">{errors.email?.message}</p>
+				</div>
+				<div className="form-control">
+					<label htmlFor="channel">Channel</label>
+					<input
+						type="text"
+						id="channel"
+						{...register("channel", { required: "Username is required" })}
+					/>
+					<p className="text-red-500">{errors.channel?.message}</p>
+				</div>
 				<div>
 					<button className="p-2 bg-blue-900 text-white rounded-md hover:bg-blue-500">
 						Submit
